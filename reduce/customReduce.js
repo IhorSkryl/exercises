d36fce36e2c932a4b23201593f78afc70fa42eda
IhorSkryl <60439020+IhorSkryl@users.noreply.github.com>
2020-02-05 17:19:16 +0200

    Add files via upload

--- reduce/customReduce.js
+++ reduce/customReduce.js
@@ -1,35 +1,36 @@
 var numArr = [1, 2, 3, 4, 5];
 
 Array.prototype.customReduce = function(fn, initialValue) {
-   var count = initialValue;
+   var accumulator = initialValue === undefined ? this[0] : initialValue;
 
-   for (var i = 0; i < this.length; i++) {
-      count = fn(count, this[i], i, this);
+   var int = initialValue === undefined ? 1 : 0;
+   for (var i = int; i < this.length; i++) {
+      accumulator = fn(accumulator, this[i], i, this);
    }
-   return count;
+
+   return accumulator;
 }
 
 
-var num = numArr.customReduce((sum, current) => {
-   return sum + current;
-}, 0)
-
+var num = numArr.customReduce((accumulator, item) => {
+   return accumulator + item;
+}, 0);
 console.log(num);
 
 const arrayA = [1, 2, 3, 4];
-const arrayB = arrayA.customReduce((accumulator, item) => accumulator += `_${item}`);
+const arrayB = arrayA.customReduce((accumulator, item) => (accumulator += `_${item}`), );
 console.log(arrayB);
 
-// const arrayA = [1, 2, 3, 4];
-// const arrayB = arrayA.customReduce(
-//    (accumulator, item) => (accumulator += `_${item}`),
-//    ""
-// );
-// console.log(arrayB);
+const arrayC = [1, 2, 3, 4];
+const arrayD = arrayC.customReduce(
+   (accumulator, item) => (accumulator += `_${item}`),
+   ""
+);
+console.log(arrayD);
 
-// const arrayA = [{ a: 1 }, { a: 2 }, { a: 3 }, { a: 4 }];
-// const arrayB = arrayA.customReduce(
-//    (accumulator, item) => (accumulator += `_${item.a}`),
-//    ""
-// );
-// console.log(arrayB);
+const arrayE = [{ a: 1 }, { a: 2 }, { a: 3 }, { a: 4 }];
+const arrayF = arrayE.customReduce(
+   (accumulator, item) => (accumulator += `_${item.a}`),
+   ""
+);
+console.log(arrayF);
